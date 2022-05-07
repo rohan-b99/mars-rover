54f8e5e0eef208daecf7ddf5dd7e7d82425193c0
rohan-b99 <rohan-b99@users.noreply.github.com>
2022-05-07 11:10:08 +0100

    Finish implementation + tests

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -8,6 +8,111 @@ defmodule MarsRover do
   returning the transformed output string
   """
   def run(input) do
-    input
+    %{width: width, height: height, commands: commands} = parse_input(input)
+    process_transforms(width, height, commands)
   end
+
+  def parse_input(input) do
+    [map_size | commands] = String.split(input, "\n")
+    [width, height] = map_size |> String.split(" ") |> Enum.map(&String.to_integer/1)
+
+    %{commands: Enum.map(commands, &parse_command/1), width: width, height: height}
+  end
+
+  @type command :: %{
+          x: integer,
+          y: integer,
+          direction: String.t(),
+          movements: list(String.t())
+        }
+
+  @doc """
+  Parse a command line from the input, consisting of the current state and a list
+  of movements
+  """
+  @spec parse_command(String.t()) :: command()
+  def parse_command(command) do
+    # Alternatively:
+    # Regex.named_captures(
+    #   ~r/\((?<x>\d+), (?<y>\d+), (?<direction>N|E|S|W)\) (?<movements>.+)/,
+    #   command
+    # )
+    with "(" <> rest <- command,
+         {x, rest} <- Integer.parse(rest),
+         ", " <> rest <- rest,
+         {y, rest} <- Integer.parse(rest),
+         ", " <> rest <- rest,
+         {direction, rest} <- String.split_at(rest, 1),
+         ") " <> movements <- rest do
+      %{x: x, y: y, direction: direction, movements: String.graphemes(movements)}
+    end
+  end
+
+  @type robot_state :: {integer, integer, String.t(), boolean()}
+
+  @doc """
+  Process each of the commands, applying each transform to the initial state,
+  returning a formatted string of the new state for each robot
+  """
+  @spec process_transforms(integer, integer, list(command)) :: String.t()
+  def process_transforms(width, height, commands) do
+    Enum.map(commands, fn command ->
+      Enum.reduce(
+        command.movements,
+        {command.x, command.y, command.direction, false},
+        fn movement, state ->
+          case process_transform(movement, state) do
+            # If the robot is out of bounds, mark as lost
+            {x, y, _, _} when x > width or x < 0 or y > height or y < 0 ->
+              put_elem(state, 3, true)
+
+            valid ->
+              valid
+          end
+        end
+      )
+    end)
+    |> Enum.map(fn {x, y, direction, lost?} ->
+      "(#{x}, #{y}, #{direction})#{if lost?, do: " LOST", else: ""}"
+    end)
+    |> Enum.join("\n")
+  end
+
+  @doc """
+  Apply a movement transformation to a state.
+  If the robot is lost, no transform is processed.
+  """
+  @spec process_transform(String.t(), robot_state) :: robot_state
+  def process_transform(_, {x, y, direction, true}), do: {x, y, direction, true}
+
+  def process_transform(movement, {x, y, direction, lost?}) do
+    case movement do
+      "L" ->
+        {x, y, turn("L", direction), lost?}
+
+      "R" ->
+        {x, y, turn("R", direction), lost?}
+
+      "F" ->
+        case direction do
+          "N" -> {x, y + 1, direction, lost?}
+          "E" -> {x + 1, y, direction, lost?}
+          "S" -> {x, y - 1, direction, lost?}
+          "W" -> {x - 1, y, direction, lost?}
+        end
+    end
+  end
+
+  @doc """
+  Transform a direction string either left or right
+  """
+  @spec turn(String.t(), String.t()) :: String.t()
+  def turn("L", "N"), do: "W"
+  def turn("L", "E"), do: "N"
+  def turn("L", "S"), do: "E"
+  def turn("L", "W"), do: "S"
+  def turn("R", "N"), do: "E"
+  def turn("R", "E"), do: "S"
+  def turn("R", "S"), do: "W"
+  def turn("R", "W"), do: "N"
 end
--- test/mars_rover_test.exs
+++ test/mars_rover_test.exs
@@ -3,17 +3,74 @@ defmodule MarsRoverTest do
   doctest MarsRover
 
   test "produces correct output" do
-    input = """
-    4 8
-    (2, 3, E) LFRFF
-    (0, 2, N) FFLFRFF
-    """
+    input =
+      """
+      4 8
+      (2, 3, E) LFRFF
+      (0, 2, N) FFLFRFF
+      """
+      |> String.trim()
 
-    expected = """
-    (4, 4, E)
-    (0, 4, W) LOST
-    """
+    expected =
+      """
+      (4, 4, E)
+      (0, 4, W) LOST
+      """
+      |> String.trim()
 
-    assert MarsRover.run(input) == output
+    assert MarsRover.run(input) == expected
+  end
+
+  test "produces correct output (example 2)" do
+    input =
+      """
+      4 8
+      (2, 3, N) FLLFR
+      (1, 0, S) FFRLF
+      """
+      |> String.trim()
+
+    expected =
+      """
+      (2, 3, W)
+      (1, 0, S) LOST
+      """
+      |> String.trim()
+
+    assert MarsRover.run(input) == expected
+  end
+
+  test "processes transform" do
+    assert MarsRover.process_transform("L", {0, 0, "N", false}) == {0, 0, "W", false}
+    assert MarsRover.process_transform("F", {2, 3, "E", false}) == {3, 3, "E", false}
+  end
+
+  test "parses input" do
+    input =
+      """
+      4 8
+      (2, 3, N) FLLFR
+      (1, 0, S) FFRLF
+      """
+      |> String.trim()
+
+    assert MarsRover.parse_input(input) == %{
+             commands: [
+               %{
+                 direction: "N",
+                 movements: ["F", "L", "L", "F", "R"],
+                 x: 2,
+                 y: 3
+               },
+               %{
+                 direction: "S",
+                 movements: ["F", "F", "R", "L", "F"],
+                 x: 1,
+                 y: 0
+               }
+             ],
+             height: 8,
+             width: 4
+           }
   end
 end
